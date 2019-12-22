0dc003cbec883ee35c5243206e15ae46f89282a8
15765-akaha <turning2012@gmail.com>
2019-12-22 15:08:30 +0900

    [Finish]3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,23 +1,18 @@
 class TodolistsController < ApplicationController
-  def new
-  	#Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
-  	@list = List.new
-
-  end
+ def new
+ 	@list = List.new
+ end
 
   def create
-
-  		list = List.new(list_params)
-
-  		list.save
-
-  		redirect_to '/top'
+  	list = List.new(list_params)
+  	list.save
+  	redirect_to '/top'
   end
   private
 
 
-  def list-params
-  		params.require(:list).permit(:title, :body)
+  def list_params
+  	  params.require(:list).permit(:title, :body)
   end
 
 

12cf9f09748cf38a397b3b042035f195d08e2616
15765-akaha <turning2012@gmail.com>
2019-12-22 20:38:05 +0900

    [finish]3-8

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -30,8 +30,6 @@ class TodolistsController < ApplicationController
 
 
  private
-
-
   def list_params
   	  params.require(:list).permit(:title, :body, :image)
   end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '76a6601a2312f74ec71dce7da016b5361f2ed804a429d7e27d4eee602584d17989d219bad6f73aea53e63766b8477f73e19551919c712681e36e783fc5f0f300'
